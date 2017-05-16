2297036cd06b51464ad327172c2a90dcb375ac5a
Ardi Mehist <ardi@mehist.com>
2017-05-16 11:30:23 +0100

    update endpoint

--- le_lambda.py
+++ le_lambda.py
@@ -20,7 +20,7 @@ logger.info('Loading function...')
 s3 = boto3.client('s3')
 
 REGION = os.environ.get('region')
-ENDPOINT = 'data.{}.logentries.com'.format(REGION)
+ENDPOINT = '{}.data.logs.insight.rapid7.com'.format(REGION)
 PORT = 20000
 TOKEN = os.environ.get('token')
 
